3dec0f568d3f92cd1ca6ac343534ff36688440cb
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-08-19 16:16:13 +0200

    chnged tabs to spaces in CalculateStringRotation

--- src/CalculateStringRotation.java
+++ src/CalculateStringRotation.java
@@ -1,18 +1,17 @@
-public class CalculateStringRotation{
-	public static void main(String[] args){
-		System.out.println(calculateStringRotation("coffee", "eecoff"));
-		System.out.println(calculateStringRotation("eecoff", "coffee"));
-		System.out.println(calculateStringRotation("moose", "Moose"));
-		System.out.println(calculateStringRotation("isn't", "'tisn"));
-		System.out.println(calculateStringRotation("Esham", "Esham"));
-		System.out.println(calculateStringRotation("dog", "god"));
-	}
-	
-	private static int calculateStringRotation(String b, String a){
-		for (int i=0; i<a.length(); i++)
-			if (b.startsWith(a.substring(i)))
-				if ((a.substring(i) + a.substring(0,i)).equals(b))
-					return i;
-		return -1;
-	}
+public class CalculateStringRotation {
+    public static void main(String[] args) {
+        System.out.println(calculateStringRotation("coffee", "eecoff"));
+        System.out.println(calculateStringRotation("eecoff", "coffee"));
+        System.out.println(calculateStringRotation("moose", "Moose"));
+        System.out.println(calculateStringRotation("isn't", "'tisn"));
+        System.out.println(calculateStringRotation("Esham", "Esham"));
+        System.out.println(calculateStringRotation("dog", "god"));
+    }
+
+    private static int calculateStringRotation(String b, String a) {
+        for (int i = 0; i < a.length(); i++)
+            if (b.startsWith(a.substring(i)))
+                if ((a.substring(i) + a.substring(0, i)).equals(b)) return i;
+        return -1;
+    }
 }
---
+++ src/MostProfit.java
@@ -0,0 +1,10 @@
+public class MostProfit{
+	public static void main(String[] args){
+	System.out.println(mostProfit({ 1, 2, 3, 4, 5, 6 }));
+	}
+	
+	private static int mostProfit(int[] quotes){
+		
+		return 0;
+	}
+}
