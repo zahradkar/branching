1d2f63804cb2316612a5519fe39aff949975778c
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-08-19 15:47:35 +0200

    refactored CalculateStringRotation

--- src/CalculateStringRotation.java
+++ src/CalculateStringRotation.java
@@ -9,14 +9,10 @@ public class CalculateStringRotation{
 	}
 	
 	private static int calculateStringRotation(String b, String a){
-		String temp = "";
-		int i = 0;
-		for (; i<a.length(); i++)
-			if (b.startsWith(a.substring(i))){
-				temp = a.substring(i) + a.substring(0,i);
-				if (temp.equals(b))
+		for (int i=0; i<a.length(); i++)
+			if (b.startsWith(a.substring(i)))
+				if ((a.substring(i) + a.substring(0,i)).equals(b))
 					return i;
-			}
 		return -1;
 	}
 }
