712bc247a54e372493f62b759842f82ca9881b94
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-08-20 19:51:56 +0200

    added HighestScoringWord

--- src/MostProfit.java
+++ src/MostProfit.java
@@ -1,6 +1,6 @@
 public class MostProfit{
 	public static void main(String[] args){
-	System.out.println(mostProfit({ 1, 2, 3, 4, 5, 6 }));
+//	System.out.println(mostProfit({ 1, 2, 3, 4, 5, 6 }));
 	}
 	
 	private static int mostProfit(int[] quotes){
